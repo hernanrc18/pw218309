c572e2c6d55ad4e3f3e945b2828fd9990d4d4992
Hernan Rosales <43820288+hernanrc18@users.noreply.github.com>
2018-10-31 09:59:27 -0700

    Programa 01 De JavaScript
    Programa de ejemplo realizado en clase. Leccion01

---
+++ Programa01 JavaScript/calculadora.js
@@ -0,0 +1,8 @@
+<script>
+		var colorAmarillo = function() {
+			operando1.style.background = "Yellow";
+		}
+		var colorblanco = function() {
+			operando1.style.background = "white";
+		}
+	</script>
