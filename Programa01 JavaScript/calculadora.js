37fd7890f9a177ab8bc4f423ee5c90495e521187
Hernan Rosales <43820288+hernanrc18@users.noreply.github.com>
2018-11-01 09:58:04 -0700

    avances js
    avances

--- Programa01 JavaScript/calculadora.js
+++ Programa01 JavaScript/calculadora.js
@@ -1,8 +1,41 @@
-<script>
-		var colorAmarillo = function() {
-			operando1.style.background = "Yellow";
+		window.onload=function(){
+		var coloramarillo = function() {
+			this.style.background = "Yellow";
 		}
 		var colorblanco = function() {
-			operando1.style.background = "white";
+			this.style.background = "white";
 		}
-	</script>
+		var operando1=document.getElementById("operando1");
+		operando1.addEventListener("focus",coloramarillo);
+		operando1.addEventListener("focusout",colorblanco)
+
+		var operando2=document.getElementById("operando2");
+		operando2.addEventListener("focus",coloramarillo);
+		operando2.addEventListener("focusout",colorblanco)
+
+		var operador= ""
+		var numeros= function() {
+			var valor= this.value;
+
+		if (operador=="") {
+			var valorInput=document.getElement("operando1").value;
+			if (valorInput==0) {
+				document.getElementById("operando1").value="";
+			}
+				document.getElementById("operando1").value+=valor;
+			
+		}
+		else {
+			var valorInput=document.getElement("operando2").value;
+			if (valorInput==0) {
+				document.getElementById("operando2").value="";
+			}
+				document.getElementById("operando2").value+=valor;
+			
+		}
+		}
+		var uno=document.getElementById("uno")
+		uno.addEventListener("Click",numeros)
+
+}
+		
