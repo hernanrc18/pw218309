57aa6f89b30ec4bbf5434a888f5139304746c856
Hernan Rosales <43820288+hernanrc18@users.noreply.github.com>
2018-11-06 09:55:30 -0700

    Programa 01 terminado js
    avances en clases de programa 01 javascript ya terminado

--- Programa01 JavaScript/calculadora.js
+++ Programa01 JavaScript/calculadora.js
@@ -1,41 +1,97 @@
 		window.onload=function(){
+		var operador=""
 		var coloramarillo = function() {
 			this.style.background = "Yellow";
 		}
 		var colorblanco = function() {
 			this.style.background = "white";
 		}
-		var operando1=document.getElementById("operando1");
-		operando1.addEventListener("focus",coloramarillo);
-		operando1.addEventListener("focusout",colorblanco)
 
-		var operando2=document.getElementById("operando2");
-		operando2.addEventListener("focus",coloramarillo);
-		operando2.addEventListener("focusout",colorblanco)
-
-		var operador= ""
-		var numeros= function() {
-			var valor= this.value;
-
-		if (operador=="") {
-			var valorInput=document.getElement("operando1").value;
-			if (valorInput==0) {
+		var numeros = function () {
+			var op1= document.getElementById('operando1').value;
+			var op2= document.getElementById('operando2').value;
+			if (operador == "") {
+				if (op1 == "0"){
 				document.getElementById("operando1").value="";
 			}
-				document.getElementById("operando1").value+=valor;
-			
+			document.getElementById("operando1").value+=this.value;
 		}
 		else {
-			var valorInput=document.getElement("operando2").value;
-			if (valorInput==0) {
+			if (op2== "0") {
 				document.getElementById("operando2").value="";
 			}
-				document.getElementById("operando2").value+=valor;
-			
+			document.getElementById("operando2").value+=this.value;
 		}
 		}
-		var uno=document.getElementById("uno")
-		uno.addEventListener("Click",numeros)
+		var operadores =function () {
+			operador =this.value
+		}
+
+		var resultadoigual = function () {
+			oper1=document.getElementById ('operando1').value
+			oper2=document.getElementById ('operando2').value
+			document.getElementById ('resultado').value=eval(oper1+operador+oper2)
+
+		}
+		var reinicio =function () {
+			document.getElementById('operando1').value="0"
+			document.getElementById('operando2').value="0"
+			document.getElementById('resultado').value="0"
+			operador=""
+		}
+ 
+ 		var operando1 = document.getElementById("operando1")
+		var operando2 = document.getElementById("operando2")
+		var resultado = document.getElementById("resultado")
+		var cero = document.getElementById("cero")
+		var uno= document.getElementById("uno")
+		var dos= document.getElementById("dos")
+		var tres= document.getElementById("tres")
+		var cuatro= document.getElementById("cuatro")
+		var cinco= document.getElementById("cinco")
+		var seis= document.getElementById("seis")
+		var siete= document.getElementById("siete")
+		var ocho= document.getElementById("ocho")
+		var nueve= document.getElementById("nueve")
+		var suma= document.getElementById("suma")
+		var resta= document.getElementById("resta")
+		var multiplicacion= document.getElementById("multiplicacion")
+		var division= document.getElementById("division")
+		var igual= document.getElementById("igual")
+		var reiniciar = document.getElementById("reiniciar")
+
+		operando1.addEventListener('focus',coloramarillo)
+		operando1.addEventListener('focusout',colorblanco)
+		operando2.addEventListener('focus',coloramarillo)
+		operando2.addEventListener('focusout',colorblanco)
+		resultado.addEventListener('focus',coloramarillo)
+		resultado.addEventListener('focusout',colorblanco)
+		reiniciar.addEventListener('click',reinicio)
+
+		cero.addEventListener('click',numeros)
+		uno.addEventListener('click',numeros)
+		dos.addEventListener('click',numeros)
+		tres.addEventListener('click',numeros)
+		cuatro.addEventListener('click',numeros)
+		cinco.addEventListener('click',numeros)
+		seis.addEventListener('click',numeros)
+		siete.addEventListener('click',numeros)
+		ocho.addEventListener('click',numeros)
+		nueve.addEventListener('click',numeros)
+		
+		suma.addEventListener('click',operadores)
+		resta.addEventListener('click',operadores)
+		multiplicacion.addEventListener('click',operadores)
+		division.addEventListener('click',operadores)
+
+		igual.addEventListener('click',resultadoigual)
+
+
+
+
 
 }
-		
+
+
+
+
